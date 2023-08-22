fc396abb65c09ccfa0738b9d49eba749c7a0a2cf
mnpin12 <mnpine12@gmail.com>
2023-08-22 18:01:29 +0900

    feature : 예매 페이지 영화 선택 부분 작성중 ver.2

--- src/components/booking/BodyMovie.js
+++ src/components/booking/BodyMovie.js
@@ -5,8 +5,13 @@ import Typography from '@mui/material/Typography';
 import {
   bcmContainerBox,
   bcmGridItem,
+  bcmGridItemImageBox,
+  bcmGridItemImageBox_none,
+  bcmGridItemImgTypo,
   bcmGridItemList,
   bcmGridItemTypo,
+  bcmGridListItemImage,
+  bcmImage,
   bcmInnerContainerBox,
   bcmListItemButton,
 } from '../../css/BookingStyles';
@@ -18,13 +23,23 @@ const movies = [
   { title: '메가로돈2', ageRating: '15세' },
   { title: '콘크리트 유토피아', ageRating: '15세' },
   { title: '밀수', ageRating: '15세' },
+  { title: '미션 임파서블: 데드 레코닝 PART ONE', ageRating: '15세' },
   { title: '엘리멘탈', ageRating: 'ALL' },
   { title: '명탐정 코난', ageRating: '12세' },
+  { title: '[2023 베로나 오페라 페스티벌] 토스카', ageRating: '12세' },
+  { title: '볼코노프 대위 탈출하다', ageRating: '15세' },
+  { title: '고대 이집트 세계관과 불변의 상징들', ageRating: 'ALL' },
+  { title: '[존 크랑코’s 발레 3부작] 로미오와 줄리엣', ageRating: 'ALL' },
+  { title: '고대 이집트 문명의 빛나는 유산', ageRating: 'ALL' },
+  { title: '스파이더맨: 어크로스 더 유니버스', ageRating: 'ALL' },
+  { title: '강변의 무코리타', ageRating: '12세' },
+  { title: '지옥만세', ageRating: '12세' },
 ];
 
 export const BodyMovie = () => {
   const [selectedMenu, setSelectedMenu] = useState(0);
   const [selectedMovies, setSelectedMovies] = useState([]);
+  const [imgUrl, setImgUrl] = useState('/images/poster_00.jpg');
 
   //영화 선택
   const handleMovieClick = (index) => {
@@ -40,7 +55,7 @@ export const BodyMovie = () => {
       <Box sx={bcmInnerContainerBox}>
         <Grid container>
           <Grid item xs={12} sm={12} sx={bcmGridItemTypo}>
-            <Typography variant='h6'>영화</Typography>
+            <Typography variant='text'>영화</Typography>
           </Grid>
           <Grid item xs={12} sm={12}>
             <List sx={bcmGridItemList}>
@@ -65,7 +80,7 @@ export const BodyMovie = () => {
             </List>
           </Grid>
           <Grid item xs={12} sm={12} sx={bcmGridItem}>
-            <List>
+            <List sx={bcmGridListItemImage}>
               {movies.map((movie, index) => (
                 <BodyMovieListButton
                   movie={movie.title}
@@ -77,7 +92,63 @@ export const BodyMovie = () => {
               ))}
             </List>
           </Grid>
-          <Grid item xs={12} sm={12} sx={bcmGridItem}></Grid>
+          <Grid item xs={12} sm={12} sx={bcmGridItem}>
+            <Box
+              sx={{
+                ...bcmGridItemImageBox_none,
+                border:
+                  selectedMovies.length !== 0 ? 'none' : '1px solid #d8d9db',
+              }}
+            >
+              {selectedMovies.length === 0 ? (
+                <Typography sx={bcmGridItemImgTypo}>
+                  모든영화
+                  <br /> 목록에서 영화를 선택하세요.
+                </Typography>
+              ) : (
+                <Box sx={bcmGridItemImageBox}>
+                  {selectedMovies.length === 1 ? (
+                    <img
+                      src={imgUrl}
+                      alt={selectedMovies[0]}
+                      style={bcmImage}
+                    />
+                  ) : selectedMovies.length === 2 ? (
+                    <>
+                      <img
+                        src={imgUrl}
+                        alt={selectedMovies[0]}
+                        style={bcmImage}
+                      />
+                      <img
+                        src={imgUrl}
+                        alt={selectedMovies[1]}
+                        style={bcmImage}
+                      />
+                    </>
+                  ) : (
+                    <>
+                      <img
+                        src={imgUrl}
+                        alt={selectedMovies[0]}
+                        style={bcmImage}
+                      />
+                      <img
+                        src={imgUrl}
+                        alt={selectedMovies[1]}
+                        style={bcmImage}
+                      />
+                      <img
+                        src={imgUrl}
+                        alt={selectedMovies[2]}
+                        style={bcmImage}
+                      />
+                    </>
+                  )}
+                </Box>
+              )}
+            </Box>
+          </Grid>
         </Grid>
       </Box>
     </Box>
