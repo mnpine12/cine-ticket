ae0bdb24a526a8d3c99effed6c9fa51942b7f7e8
mnpin12 <mnpine12@gmail.com>
2023-08-22 11:25:36 +0900

    feature : 예매 페이지 calendar 부분 작성 완료

--- src/components/booking/HeaderCalendar.js
+++ src/components/booking/HeaderCalendar.js
@@ -32,7 +32,7 @@ function ButtonField(props) {
       sx={bmTopCalendarButton}
       onClick={() => setOpen?.((prev) => !prev)}
     >
-      <CalendarMonthIcon />
+      <CalendarMonthIcon sx={{ padding: 0 }} />
     </Button>
   );
 }
@@ -65,19 +65,12 @@ const getTwoWeeksFromToday = () => {
   let currentDate = today.clone();
   while (currentDate.isBefore(twoWeeksLater)) {
     const formattedDate = currentDate.format('YYYY-MM-DD');
-    const formattedDate2 = formattedDate.split('-');
+    const newFormattedDate = formattedDate.split('-');
     const dayOfWeek = daysOfWeek[currentDate.day()];
-    const formattedDateWithDay = `${formattedDate}-${dayOfWeek}`;
-    const formattedDateWithDayText = `${formattedDate2[2]}·${dayOfWeek}`;
-
-    // 객체로 날짜 정보 포장하여 배열에 추가
-    const dateObject = {
-      date: formattedDateWithDay,
-      dateText: formattedDateWithDayText,
-    };
+    const formattedDateWithDayText = `${newFormattedDate[2]}·${dayOfWeek}`;
 
     datesText.push(formattedDateWithDayText);
-    dates.push(dateObject);
+    dates.push(formattedDate);
     currentDate = currentDate.add(1, 'day');
   }
 
@@ -86,48 +79,59 @@ const getTwoWeeksFromToday = () => {
 
 // HeaderCalendar
 export const HeaderCalendar = () => {
-  const today = dayjs();
+  const today = dayjs().format('YYYY-MM-DD');
   const [selectedCalendarDate, setSelectedCalendarDate] = useState(null);
-  const [selectedDay, setSelectedDay] = useState(0);
+  const [selectedDay, setSelectedDay] = useState(today);
+  const [selectedDayIndex, setSelectedDayIndex] = useState(0);
 
   const { dates, datesText } = getTwoWeeksFromToday();
 
   const isYesterday = (date) => {
-    return dayjs(dayjs(date).format('YYYY-MM-DD')).isBefore(
-      today.format('YYYY-MM-DD'),
-    );
+    return dayjs(dayjs(date).format('YYYY-MM-DD')).isBefore(today);
   };
 
-  const handleDayListClick = (event, index) => {
-    setSelectedDay(index);
+  const handleDayListClick = (date, index) => {
+    setSelectedDayIndex(index);
+    setSelectedDay(date);
+    console.log(date);
+  };
+
+  const handleCalendarDateClick = (newDate) => {
+    setSelectedCalendarDate(newDate.format('YYYY-MM-DD'));
   };
 
   return (
     <Box sx={bmTopCalendarBox}>
       <Button disableRipple={true} sx={bmTopCalendarButton}>
-        <NavigateBeforeIcon />
+        <NavigateBeforeIcon sx={{ padding: 0 }} />
       </Button>
       <List sx={bmListDates}>
         {dates.map((date, index) => (
           <ListItemButton
             key={index}
-            selected={selectedDay === index}
-            onClick={(event) => handleDayListClick(event, index)}
-            sx={bmListDatesItemButton}
+            value={selectedDay}
+            selected={selectedDayIndex === index}
+            onClick={() => handleDayListClick(date, index)}
+            sx={{
+              ...bmListDatesItemButton,
+              color: datesText[index].includes('일')
+                ? 'red'
+                : datesText[index].includes('토')
+                ? 'blue'
+                : 'inherit',
+            }}
           >
             {datesText[index]}
           </ListItemButton>
         ))}
       </List>
       <Button disableRipple={true} sx={bmTopCalendarButton}>
-        <NavigateNextIcon />
+        <NavigateNextIcon sx={{ padding: 0 }} />
       </Button>
       <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale='ko'>
         <ButtonDatePicker
           value={selectedCalendarDate}
-          onChange={(newDate) =>
-            setSelectedCalendarDate(newDate.format('YYYY-MM-DD'))
-          }
+          onChange={(newDate) => handleCalendarDateClick(newDate)}
           isYesterday={isYesterday}
         />
       </LocalizationProvider>
--- src/css/BookingStyles.js
+++ src/css/BookingStyles.js
@@ -38,12 +38,16 @@ export const bmTitleTypo = {
 export const bmTopCalendarBox = {
   width: '100%',
   height: '40px',
+  margin: 0,
+  padding: 0,
   borderTop: '1px solid #555555',
   borderLeft: '1px solid #d8d9db',
   borderRight: '1px solid #d8d9db',
+  display: 'flex',
 };
 
 export const bmTopCalendarButton = {
+  minWidth: '10px',
   height: '39px',
   color: '#898989',
   '&:hover': {
@@ -52,14 +56,24 @@ export const bmTopCalendarButton = {
 };
 
 export const bmListDates = {
+  width: '100%',
+  height: '100%',
   display: 'flex',
   flexDirection: 'row',
+  alignItems: 'center',
   justifyContent: 'space-between',
   margin: 0,
   padding: 0,
 };
 
 export const bmListDatesItemButton = {
-  height: '39px',
-  fontSize: '12px',
+  fontFamily: 'NanumBarunGothic, sans-serif',
+  fontSize: '15px',
+  padding: '0', // List 안에 꽉차도록 조절
+  width: '100%',
+  height: '100%',
+  boxSizing: 'border-box',
+  display: 'flex', // 이 아래로 글자 가운데 정렬
+  alignItems: 'center',
+  justifyContent: 'center',
 };
