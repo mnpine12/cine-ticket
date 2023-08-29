559dc3f9974050b7700eb5bb1a2381e1023501b0
mnpin12 <mnpine12@gmail.com>
2023-08-29 17:48:20 +0900

    feature : 영화 예매 페이지 좌석 선택 작성중

--- src/components/seatBooking/SeatSelector.js
+++ src/components/seatBooking/SeatSelector.js
@@ -1,57 +1,15 @@
 import React from 'react';
-
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import Grid from '@mui/material/Grid';
-import LoopIcon from '@mui/icons-material/Loop';
 
-import {
-  ssCountButton,
-  ssOuterBox,
-  ssTopButton,
-  ssTopGridContainer,
-  ssTopTitleTypo,
-} from '../../css/SeatBookingStyles';
+import { ssOuterBox } from '../../css/SeatBookingStyles';
+import { SpectatorCounter } from './SpectatorCounter';
+import { SeatDisplay } from './SeatDisplay';
 
 export const SeatSelector = () => {
   return (
     <Box sx={ssOuterBox}>
-      <Grid container sx={ssTopGridContainer}>
-        <Grid item xs={12} sm={6}>
-          <Typography sx={ssTopTitleTypo}>관람인원선택</Typography>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          sm={6}
-          sx={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-          }}
-        >
-          <Button
-            disableElevation
-            disableRipple
-            variant='contained'
-            size='small'
-            sx={ssTopButton}
-          >
-            <LoopIcon />
-            초기화
-          </Button>
-        </Grid>
-        <Grid item xs={12} sm={12}>
-          <Button
-            disableElevation
-            disableRipple
-            variant='contained'
-            sx={ssCountButton}
-          >
-            -
-          </Button>
-        </Grid>
-      </Grid>
+      <SpectatorCounter />
+      <SeatDisplay />
     </Box>
   );
 };
